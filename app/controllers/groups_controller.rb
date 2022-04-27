683eea2e65bed2e6074b2eb1d06238e8ea5eb678
charlesgobina <charles.gobina@gmail.com>
2022-04-27 14:54:30 +0100

    add category index page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,25 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index;
+    @groups = current_user.groups.all
+  end
 
   def new
     @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    if @group.save
+      flash[:notice] = 'Group created'
+      redirect_to groups_path
+    else
+      flash[:alert] = 'Group not created'
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
